0b005a47c713a513a29c64c23847af6f48fd72d6
neil <karta134033@gmail.com>
2020-02-21 18:04:47 +0800

    2020/02/21更改 Details:獨立出App.vue的css 避免其他components在ubuntu環境下吃不到App.vue的css

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import BottomNav from './components/BottomNav.vue'
-
+import '@/assets/css/style.css'
 export default {
   name: 'App',
   components: {
@@ -15,45 +15,3 @@ export default {
   },
 }
 </script>
-<style>
-body, html {
-  background-color: black;
-  background-image: url('./assets/beach.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-attachment: fixed;
-  background-position: center;
-}
-#app {
-  height: 100%;
-}
-.div-align-middle:before {
-  content: '';
-  display: inline-block;
-  vertical-align: middle ;
-  height: 100%;
-}
-.animate-in {
-  visibility: visible;
-  opacity: 1;
-  -webkit-transition: opacity 1s ease-out;
-  -moz-transition: opacity 1s ease-out;
-  -o-transition: opacity 1s ease-out;
-  transition: opacity 1s ease-out;
-}
-.animate-out {
-  visibility: hidden;
-  opacity: 0;
-  -webkit-transition: opacity 1s ease-out;
-  -moz-transition: opacity 1s ease-out;
-  -o-transition: opacity 1s ease-out;
-  transition: opacity 1s ease-out;
-}
-.sticky {
-	z-index: 1;
-	position: fixed;
-	top: 0;
-	width: 100%;
-}
-</style>
-
